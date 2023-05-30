e081a7408e0ec0adffeec64be1201de709e3213f
SheaDevOp <nacho.dieguez18@gmail.com>
2023-05-30 17:46:14 -0300

    mongodb connected

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,9 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 
 const app = express();
 
@@ -21,4 +22,10 @@ const server = http.createServer(app);
 
 server.listen(PORT, () => {
   console.log(`Server running on: http://localhost:${PORT}/`);
-})
+});
+
+const MONGO_URL = 'mongodb+srv://nachodieguez:yHxOVTcoEywfhjD2@cluster0.fbkdtqp.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
