7ae3d8377a7d6f1b096a7622925e78e0c80082b4
SheaDevOp <nacho.dieguez18@gmail.com>
2023-05-31 09:58:00 -0300

    auth and encryption helpers

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,8 +1,8 @@
 import crypto from 'crypto';
 
-const SECRET = 'SHEA-REST_API'
+const SECRET = 'SHEA-REST-API'
 
 export const random = () => crypto.randomBytes(128).toString('base64');
-export const authentication = (salt: string, password: string) => {
-  return crypto.createHmac('sha56', [salt, password].join('/')).update(SECRET).digest('hex');
+export const authentication = (salt: string, password: string): string => {
+  return crypto.createHmac('sha256', [salt, password].join('/')).update(SECRET).digest('hex');
 }
